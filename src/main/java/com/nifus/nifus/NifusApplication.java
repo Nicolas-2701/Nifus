8599e47d877639f21fee9a184a6f071612ed8807
Nicolas-2701 <ngalvao388@gmail.com>
2025-02-17 14:15:06 -0300

    spring initializr

---
+++ src/main/java/com/nifus/nifus/NifusApplication.java
@@ -0,0 +1,13 @@
+package com.nifus.nifus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NifusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NifusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nifus/nifus/NifusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nifus.nifus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NifusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
